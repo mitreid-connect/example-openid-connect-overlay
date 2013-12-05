55c2f8ebeadfbe1c68d8c9f87a6321e6a2aba81f
Justin Richer <jricher@mitre.org>
2013-12-05 13:04:11 -0500

    updated to 1.1

--- my-openid-connect/src/main/java/org/example/openid/connect/adaptor/model/ExampleUserInfo.java
+++ my-openid-connect/src/main/java/org/example/openid/connect/adaptor/model/ExampleUserInfo.java
@@ -5,6 +5,8 @@ import javax.persistence.OneToOne;
 import org.mitre.openid.connect.model.UserInfo;
 import org.mitre.openid.connect.model.Address;
 
+import com.google.gson.JsonObject;
+
 public class ExampleUserInfo implements UserInfo {
 
 	/**
@@ -256,6 +258,70 @@ public class ExampleUserInfo implements UserInfo {
 	 * @param birthdate
 	 */
 	public void setBirthdate(String birthdate) {}
+
+	/* (non-Javadoc)
+	 * @see org.mitre.openid.connect.model.UserInfo#getPhoneNumberVerified()
+	 */
+    @Override
+    public Boolean getPhoneNumberVerified() {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	/* (non-Javadoc)
+	 * @see org.mitre.openid.connect.model.UserInfo#setPhoneNumberVerified(java.lang.Boolean)
+	 */
+    @Override
+    public void setPhoneNumberVerified(Boolean phoneNumberVerified) {
+	    // TODO Auto-generated method stub
+	    
+    }
+
+	/* (non-Javadoc)
+	 * @see org.mitre.openid.connect.model.UserInfo#toJson()
+	 */
+    @Override
+    public JsonObject toJson() {
+		JsonObject obj = new JsonObject();
+
+		obj.addProperty("sub", this.getSub());
+
+		obj.addProperty("name", this.getName());
+		obj.addProperty("preferred_username", this.getPreferredUsername());
+		obj.addProperty("given_name", this.getGivenName());
+		obj.addProperty("family_name", this.getFamilyName());
+		obj.addProperty("middle_name", this.getMiddleName());
+		obj.addProperty("nickname", this.getNickname());
+		obj.addProperty("profile", this.getProfile());
+		obj.addProperty("picture", this.getPicture());
+		obj.addProperty("website", this.getWebsite());
+		obj.addProperty("gender", this.getGender());
+		obj.addProperty("zone_info", this.getZoneinfo());
+		obj.addProperty("locale", this.getLocale());
+		obj.addProperty("updated_time", this.getUpdatedTime());
+		obj.addProperty("birthdate", this.getBirthdate());
+		
+		obj.addProperty("email", this.getEmail());
+		obj.addProperty("email_verified", this.getEmailVerified());
+
+		obj.addProperty("phone_number", this.getPhoneNumber());
+		obj.addProperty("phone_number_verified", this.getPhoneNumberVerified());
+
+		if (this.getAddress() != null) {
+
+			JsonObject addr = new JsonObject();
+			addr.addProperty("formatted", this.getAddress().getFormatted());
+			addr.addProperty("street_address", this.getAddress().getStreetAddress());
+			addr.addProperty("locality", this.getAddress().getLocality());
+			addr.addProperty("region", this.getAddress().getRegion());
+			addr.addProperty("postal_code", this.getAddress().getPostalCode());
+			addr.addProperty("country", this.getAddress().getCountry());
+
+			obj.add("address", addr);
+		}
+
+		return obj;
+    }
 
 
 }
